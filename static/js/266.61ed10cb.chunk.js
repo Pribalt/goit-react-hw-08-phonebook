71195c002db40ce318838a8d6dd79c4ba31cff58
Pribalt <vitalii.vitalis.ua@gmail.com>
2023-04-23 10:41:01 +0000

    Deploying to gh-pages from @ Pribalt/goit-react-hw-08-phonebook@e87fabe14c42d50a1a5ce1fdb6c12c5f47f5b93d 🚀

---
+++ static/js/266.61ed10cb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(3329);function c(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{children:"Phonebook"})})}}}]);
+//# sourceMappingURL=266.61ed10cb.chunk.js.map
